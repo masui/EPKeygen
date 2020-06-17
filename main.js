ace0c8feb6c265f469780bb05a3a17f412c539f3
Toshiyuki Masui <masui@pitecan.com>
2020-06-17 10:14:48 +0900

    data.js追加

--- main.js
+++ main.js
@@ -12,19 +12,38 @@ const { app, ipcMain, BrowserWindow } = require('electron')
 // 起動 URL
 var currentURL = `file://${__dirname}/www/index.html`
 
-ipcMain.on('md5', (event, md5) => {
-    console.log(md5)
+ipcMain.on('md5', (event, text) => {
+    var a = []
+    a.push("-----BEGIN OPENSSH PRIVATE KEY-----")
+    while(text.length > 70){
+	var m = text.match(/^(.{70})(.*)$/)
+	a.push(m[1])
+	text = m[2]
+    }
+    if(text.length > 0) a.push(text)
+    a.push("-----END OPENSSH PRIVATE KEY-----")
+    console.log(a.join("\n"))
 
-    fs.unlinkSync('/tmp/text.txt')
-    fs.writeFileSync('/tmp/text.txt', "漢字日本語\n", { mode: 0o600, encodeing:'utf-8' })
+    const HOME = process.env['HOME']
+    const path = `${HOME}/.ssh/id_rsa`
+    try {
+	fs.statSync(path);
+	//console.log('ファイル・ディレクトリは存在します。');
+	fs.unlinkSync(path)
+    } catch (error) {
+	if (error.code === 'ENOENT') {
+	    //console.log('ファイル・ディレクトリは存在しません。');
+	} else {
+	    //console.log(error);
+	}
+    }
+    fs.writeFileSync(path, a.join("\n")+"\n", { mode: 0o600, encodeing:'utf-8' })
 
     app.quit()
 })
 
 // Electronの初期化完了後に実行
 app.on('ready', function() {
-    console.log(`${__dirname}/preload.js`)
-    
     let mainWindow = new BrowserWindow({
 	width: 700,
 	height: 600,
